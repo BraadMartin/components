920f992ede76bc9b6e865b41660eb6da3a333145
Sky Shabatura <gitkraken@sky.camp>
2017-11-17 10:29:39 -0600

    Adjusted control markup to work with WordPress 4.9.

--- customizer/alpha-color-picker/alpha-color-picker.php
+++ customizer/alpha-color-picker/alpha-color-picker.php
@@ -70,24 +70,28 @@ class Customize_Alpha_Color_Control extends WP_Customize_Control {
 		if ( is_array( $this->palette ) ) {
 			$palette = implode( '|', $this->palette );
 		} else {
-			// Default to true.
+			// Default to true
 			$palette = ( false === $this->palette || 'false' === $this->palette ) ? 'false' : 'true';
 		}
 
 		// Support passing show_opacity as string or boolean. Default to true.
 		$show_opacity = ( false === $this->show_opacity || 'false' === $this->show_opacity ) ? 'false' : 'true';
 
-		// Begin the output. ?>
-		<label>
-			<?php // Output the label and description if they were passed in.
-			if ( isset( $this->label ) && '' !== $this->label ) {
-				echo '<span class="customize-control-title">' . sanitize_text_field( $this->label ) . '</span>';
-			}
-			if ( isset( $this->description ) && '' !== $this->description ) {
-				echo '<span class="description customize-control-description">' . sanitize_text_field( $this->description ) . '</span>';
-			} ?>
-			<input class="alpha-color-control" type="text" data-show-opacity="<?php echo $show_opacity; ?>" data-palette="<?php echo esc_attr( $palette ); ?>" data-default-color="<?php echo esc_attr( $this->settings['default']->default ); ?>" <?php $this->link(); ?>  />
-		</label>
+		// Output the label if passed in
+		if ( isset( $this->label ) && '' !== $this->label ) {
+			echo '<span class="customize-control-title">' . sanitize_text_field( $this->label ) . '</span>';
+		}
+
+		// Output the description if passed in	
+		if ( isset( $this->description ) && '' !== $this->description ) {
+			echo '<span class="description customize-control-description">' . sanitize_text_field( $this->description ) . '</span>';
+		} 
+		?>
+		<div class="customize-control-content">
+			<label>
+				<input class="alpha-color-control" type="text" data-show-opacity="<?php echo $show_opacity; ?>" data-palette="<?php echo esc_attr( $palette ); ?>" data-default-color="<?php echo esc_attr( $this->settings['default']->default ); ?>" <?php $this->link(); ?>  />
+			</label>
+		</div>
 		<?php
 	}
 }
